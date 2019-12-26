a5e2af1f390637f34a0e4a5a24881a65a1f4301a
Sid0rr0 <elhungus@gmail.com>
2019-12-26 12:10:13 +0100

    changed App to class and added state

--- src/App.js
+++ src/App.js
@@ -4,50 +4,50 @@ import './App.css';
 import TodoItem from './components/TodoItem'
 import todosData from './data/todosData'
 
-function App() {
-  const date = new Date()
-  const hours = date.getHours()
-  let timeOfDay
-  const styles = {
-    fontSize: 30
+class App extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      todos: todosData
+    }
   }
   
-  if (hours < 12) {
-    timeOfDay = "morning"
-    styles.color = "#04756F"
-  } else if (hours >= 12 && hours < 17) {
-    timeOfDay = "afternoon"
-    styles.color = "#8914A3"
-  } else {
-    timeOfDay = "night"
-    styles.color = "#D90000"
-  }
-  const data = todosData.map(todo => <TodoItem key={todo.id} item={todo} />)
-  return (
-    <div className="todo-list">
-      <h1 style={styles}>Good {timeOfDay}!</h1>
-
-      {data}
+  date() {
+    const date = new Date()
+    const hours = date.getHours()
+    let timeOfDay
+    const styles = {
+      fontSize: 30
+    }
     
-    </div>
+    if (hours < 12) {
+      timeOfDay = "morning"
+      styles.color = "#04756F"
+    } else if (hours >= 12 && hours < 17) {
+      timeOfDay = "afternoon"
+      styles.color = "#8914A3"
+    } else {
+      timeOfDay = "night"
+      styles.color = "#D90000"
+    }
 
-    /*<div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>*/
-  );
+    return [styles, timeOfDay]
+  }
+
+  render() {
+    const data = this.state.todos.map(todo => <TodoItem key={todo.id} item={todo} />)
+    const [styles, timeOfDay] = this.date()
+    return (
+      <div className="todo-list">
+        <h1 style={styles}>Good {timeOfDay}!</h1>
+
+        {data}
+      
+      </div>
+    );
+  }
+  
 }
 
 export default App;
