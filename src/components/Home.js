27b0413092bf1a4cca004631a9c06cc7d8d312e0
Sid0rr0 <elhungus@gmail.com>
2020-01-03 02:20:50 +0100

    homepage and styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,14 +2,42 @@ import React from 'react';
 import {Link} from 'react-router-dom'
 import TodoList from './TodoList';
 import MemeGenerator from './MemeGenerator';
+import "../css/Home.css"
 
 function Home() {
   return (
-    <div>
-        <h1>Home</h1>
-        <Link to='TodoList'>Todo List</Link>
-        <Link to='/MemeGenerator'>Meme Generator</Link>
-    </div>
+    <main className="wrapper">
+        <article className="resume">
+            <section className="grid-area name">
+                Thanh Hung Le
+            </section>
+            <section className="grid-area photo">
+            <div className="circle"></div>
+            </section>
+                <section className="grid-area about">
+                <hr/>
+                <hr/>
+            </section>
+            <section className="grid-area work">
+                <hr/>
+                <hr/>
+                <hr/>
+                <hr/>
+                <hr/>
+                <hr/>
+            </section>
+            <section className="grid-area education">
+                <Link to='/MemeGenerator'> Meme Generator </Link>
+            </section>
+            <section className="grid-area skills">
+                <Link to='/TodoList'> TodoList </Link>
+            </section>
+            <section className="grid-area community">
+                
+            </section>
+        </article>
+    </main>
+   
   );
 }
 
