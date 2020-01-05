9cfc6c6a0cc8ac8f48c7cae72987a2493bf5690e
Sid0rr0 <elhungus@gmail.com>
2020-01-05 16:02:48 +0100

    styling and more content

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,28 +9,58 @@ function Home() {
     <main className="wrapper">
         <article className="resume">
             <section className="grid-area name">
-                Thanh Hung Le
+                <div className="centred-wrapper">
+                    <div className="centred">
+                        <h1>Thanh Hung Le</h1>
+                        Student at <a href="https://fit.cvut.cz/">FIT CTU Prague</a> - Software and Web Engeneering
+                    </div>
+                </div>
             </section>
             <section className="grid-area photo">
-            <div className="circle"></div>
-            </section>
-                <section className="grid-area about">
-                <hr/>
-                <hr/>
-            </section>
-            <section className="grid-area work">
-                <hr/>
-                <hr/>
-                <hr/>
-                <hr/>
-                <hr/>
-                <hr/>
-            </section>
-            <section className="grid-area education">
-                <Link to='/MemeGenerator'> Meme Generator </Link>
+                <div className="circle"></div>
             </section>
             <section className="grid-area skills">
-                <Link to='/TodoList'> TodoList </Link>
+                <div className="centred-wrapper">
+                    <div className="centred">
+                        <h3>Skills</h3>
+                        <ul>
+                            <li>HTML5 and CSS3</li>
+                            <li>Javascript (ES6)</li>
+                            <li>React</li>
+                            <li>SQL</li>
+                            <li>PHP</li>
+                            <li>C, C++</li>
+                            <li>Git</li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+            <section className="grid-area work">
+            <div className="centred-wrapper">
+                    <div className="centred">
+                        <h2>Work</h2>
+                        <Link to='/MemeGenerator'> Meme Generator </Link> <br/>
+                        <Link to='/TodoList'> TodoList </Link>
+                    </div>
+                </div>
+            </section>
+            <section className="grid-area education">
+                <div className="centred-wrapper">
+                    <div className="centred">
+                        <h3>Education</h3>
+                        Exchange student at <a href="https://www.ntu.edu.sg/Pages/home.aspx">Nanyang Technological University Singapore</a>
+                        <br />
+                        <br />
+                        Gymnazium Cheb
+                    </div>
+                </div>
+            </section>
+            <section className="grid-area about">
+                <div className="centred-wrapper">
+                    <div className="centred">
+                        <h3>About</h3>
+                    </div>
+                </div>
             </section>
             <section className="grid-area community">
                 
